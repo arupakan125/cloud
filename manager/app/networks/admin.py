51a8c99757faf2170d95907f8d356e5f1d7a70d6
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2021-05-21 23:07:39 +0900

    add network models

--- manager/app/networks/admin.py
+++ manager/app/networks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Network, Vlan
 
 # Register your models here.
+admin.site.register(Network)
+admin.site.register(Vlan)
--- manager/app/networks/models.py
+++ manager/app/networks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Network(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Vlan(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+    vlan = models.IntegerField()
+    network = models.ForeignKey(Network, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
